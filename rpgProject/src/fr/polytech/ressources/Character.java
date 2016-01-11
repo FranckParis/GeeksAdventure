d8c8f09d84d09fb08d492b63293a115d3e520e3c
Nicolas Diab <Nicolas Diab>
2016-01-11 15:16:35 +0100

    Added basic methods for items. TODO : add items stats to damage

--- rpgProject/src/fr/polytech/ressources/Character.java
+++ rpgProject/src/fr/polytech/ressources/Character.java
@@ -14,7 +14,6 @@ public abstract class Character {
     protected int mp;
     protected int armorClass;
     protected int armorBuff;
-    
     protected int level;
     protected int maxWeight;
     
@@ -24,10 +23,85 @@ public abstract class Character {
     protected Experience experience;
     protected ArrayList<CharState> charStates;
     
+    protected Weapon weapon;
+    protected Armor plackart; //plastron in french
+    protected Armor helmet;
+    protected Armor boots;
+    protected Armor trousers;
+    protected Armor gauntlet;
+    
     //Methods
     public abstract void initSkills();
     public abstract void initInventory();
     
+    //Inventory - equip method
+    public void equipItem(Item item) {
+        if (item instanceof Armor) {
+            // 0=helmet ; 1=plackart ; 2=gauntlet ; 3=trousers ; 4=boots
+            Armor armor = (Armor)item;
+            
+            switch(armor.getArmorType()) {
+                case 0:
+                    if (this.helmet != null)
+                        this.unequipHelmet();
+                    this.helmet = armor;
+                    break;
+                case 1:
+                    if (this.plackart != null)
+                        this.unequipPlackart();
+                    this.plackart = armor;
+                    break;
+                case 2:
+                    if (this.gauntlet != null)
+                        this.unequipGauntlet();
+                    this.gauntlet = armor;
+                    break;
+                case 3:
+                    if (this.trousers != null)
+                        this.unequipTrousers();
+                    this.trousers = armor;
+                    break;
+                case 4:
+                    if (this.boots != null)
+                        this.unequipBoots();
+                    this.boots = armor;
+                    break;
+            }
+        }
+        else if (item instanceof Weapon) {
+            Weapon weapon = (Weapon)item;
+            if (this.weapon != null)
+                this.unequipWeapon();
+            this.weapon = weapon;
+        }
+    }
+    
+    // Unequip methods
+    public void unequipHelmet() {
+        this.helmet = null;
+    }
+    
+    public void unequipPlackart() {
+        this.plackart = null;
+    }
+    
+    public void unequipGauntlet() {
+        this.gauntlet = null;
+    }
+    
+    public void unequipTrousers() {
+        this.trousers = null;
+    }
+    
+    public void unequipBoots() {
+        this.boots = null;
+    }
+    
+    public void unequipWeapon() {
+        this.weapon = null;
+    }
+    
+    // States
     public void applyCharStates(){
         ArrayList <Integer> toDelete = new ArrayList();
         
@@ -50,6 +124,7 @@ public abstract class Character {
         
     }
     
+    // getters
     public String getName() {
         return name;
     }
@@ -107,5 +182,31 @@ public abstract class Character {
         }
         return immune;
     }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public Armor getPlackart() {
+        return plackart;
+    }
+
+    public Armor getHelmet() {
+        return helmet;
+    }
+
+    public Armor getBoots() {
+        return boots;
+    }
+
+    public Armor getTrousers() {
+        return trousers;
+    }
+
+    public Armor getGauntlet() {
+        return gauntlet;
+    }
+    
+    
     
 }
