d2af03771d441910211e6ded595eed240277e26d
Nicolas Diab <nicolas-diab@hotmail.fr>
2016-01-18 17:26:26 +0100

    add details to TryHarder. Now playable

--- rpgProject/src/fr/polytech/ressources/TryHarder.java
+++ rpgProject/src/fr/polytech/ressources/TryHarder.java
@@ -24,6 +24,7 @@ public class TryHarder extends Charact {
         this.position = pos;
         
         this.experience = new Experience();
+        this.charStates = new ArrayList<>();
         
         this.abilityScores = new AbilityScores(10, 20, 70);
         
@@ -34,13 +35,40 @@ public class TryHarder extends Charact {
 
     @Override
     public void initSkills() {
-        //this.skills.add(new Skill ("Mandale.exe", 6, 3, 0, 0));
+        skills = new ArrayList<>();
+        
+        //Primary skills
+        this.skills.add(new Skill ("Le retour du baton !", 8, 2, 0, 2));
+        this.skills.add(new Skill ("La Parade", 0, 0, 2, new CharState("Parade", 4)));
+                
+        //Specific skills
+        this.skills.add(new Skill ("Boule de feu des familles", 10, 3, 15, 2));
+        this.skills.add(new Skill ("Heal des familles", -10, 2, 20, 2));
+        this.skills.add(new Skill ("Pression extra-orbital", 8, 2, 20, 2, new CharState("Pression extra-orbital", 8, 0, 4)));
+        this.skills.add(new Skill ("J'carry la game gros", 20, 4, 42, 2));
     }
 
     @Override
     public void initInventory() {
         this.inventory = new ArrayList<>();
         
+        Armor helmet = new Armor("Casque Turtle Beach 7.1",1,0,1,"Casque utilisé par des champions de Starcraft");
+        this.inventory.add(helmet);
+        
+        Armor plackart = new Armor("Chemise propre",1,1,1,"Chemise propre toute blanche, parfaite pour les streams");
+        this.inventory.add(plackart);
+        
+        Armor gauntlets = new Armor("Rien",0,2,0,"Nope, rien");
+        this.inventory.add(gauntlets);
+        
+        Armor trousers = new Armor("Jean propre",1,3,2,"Jean bien propre, parfait pour les longs try de raid");
+        this.inventory.add(trousers);
+        
+        Armor boots = new Armor("Chaussettes sales",0,4,3,"C'est le poids de la culpabilité");
+        this.inventory.add(boots);
+        
+        Weapon weapon = new Weapon("Brindille de bois",6,2,2,"VOUS NE PASSEREZ ... non .. attendez ..");
+        this.inventory.add(weapon);
     }
     
     @Override
